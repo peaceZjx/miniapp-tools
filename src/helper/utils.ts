a00c86ec2953ecf2893a7b3374be9edc8170bb9a
zjx <zjx@yuxisoft.cn>
2020-06-16 13:24:10 +0800

    feat: 新增小程序的promise化工具函数

--- src/helper/utils.ts
+++ src/helper/utils.ts
@@ -36,6 +36,21 @@ export function isArray(val: any): val is any[] {
 	return toString.call(val, null) === '[object Array]'
 }
 
-export function sd() {
-	console.log(1)
+/**
+ * 将小程序的异步回调api给promise化
+ * @param func 被promise化的函数
+ * @param args 传递给这个函数的参数
+ */
+export const promisely = (func: any, args?: any) => {
+	if (typeof func !== 'function') {
+		return console.warn('[promisely] 第一个参数是函数')
+	}
+	return new Promise((resolve, reject) => {
+		func({
+			...args,
+			success: resolve,
+			fail: reject,
+			complete: resolve,
+		})
+	})
 }
