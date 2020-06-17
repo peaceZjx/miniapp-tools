0e53fe51eea676afa37a1dfcbc7a88eef9de775a
zjx <zjx@yuxisoft.cn>
2020-06-17 09:18:33 +0800

    feat: 加入简单的队列方法

--- src/common/index.ts
+++ src/common/index.ts
@@ -35,3 +35,63 @@ export function isPlainObject(val: any): val is Object {
 export function isArray(val: any): val is any[] {
 	return toString.call(val, null) === '[object Array]'
 }
+
+export class Queue {
+	public queue: any[]
+	constructor() {
+		this.queue = []
+	}
+
+	/**
+	 * 入队
+	 */
+	enqueue(arg: any) {
+		this.queue.push(arg)
+	}
+
+	/**
+	 * 出队
+	 */
+	dequeue() {
+		let result = this.queue.shift()
+		return typeof result !== 'undefined' ? result : false
+	}
+
+	/**
+	 * 队列是否为空
+	 */
+	isEmpty() {
+		return this.queue.length === 0
+	}
+
+	/**
+	 * 返回队列长度
+	 */
+	size() {
+		return this.queue.length
+	}
+
+	/**
+	 * 清空队列
+	 */
+	clear() {
+		this.queue = []
+	}
+
+	/**
+	 * 将整个队列返回，队列不会清空
+	 */
+	show() {
+		return this.queue
+	}
+
+	/**
+	 * 出队并且执行，直到清空队列
+	 */
+	loopExec() {
+		if (this.isEmpty()) return
+		let element = this.dequeue()
+		typeof element === 'function' && element()
+		this.loopExec()
+	}
+}
